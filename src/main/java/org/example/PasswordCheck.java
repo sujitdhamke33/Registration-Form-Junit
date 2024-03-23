24b6d41649caefd6db3cd5b5340e6401d71aaba8
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-23 14:57:24 +0530

    UC10

--- src/main/java/org/example/PasswordCheck.java
+++ src/main/java/org/example/PasswordCheck.java
@@ -5,23 +5,65 @@ import java.util.regex.Pattern;
 
 public class PasswordCheck {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter All email test cases to satisfy the conditions given");
+        System.out.println("Enter the First name of user :");
+        String Firstname = sc.next();
+        System.out.println("Enter the last name of user :");
+        String Lastname = sc.next();
+        System.out.println("Enter Email id : ");
+        String email = sc.next();
+        sc.nextLine();
+        System.out.println("Enter phone number along with country code ");
+        String number = sc.nextLine();
+        System.out.println("enter the password ");
         String password = sc.next();
 
-        if(passwordAllEmailCheck(password)){
-            System.out.println("The password enter to test is correct " + password);
+        if (isvalidname(Firstname)) {
+            System.out.println(Firstname+" successfully registered ");
+        } else {
+            System.out.println("First name is invalid ");
         }
-        else{
-            System.out.println("Entered password is invalid ");
+        if (isvalidname(Lastname)) {
+            System.out.println(Lastname+ " successfully registered ");
+        } else {
+            System.out.println("Last name is invalid ");
+        }
+        if(validemail(email)){
+            System.out.println(email+ " Email id registered  ");
+        }
+        else {
+            System.out.println("Email is invalid ");
+        }
+        if(validnumber(number)){
+            System.out.println(number+"  successfully registered ");
+        }
+        else {
+            System.out.println("Enter phone number in proper format 10 digits number with country code ");
+        }
+        if(validpassword(password)){
+            System.out.println("Password successfully registered ");
+        }
+        else {
+            System.out.println("Password is not following standard rules  ");
         }
 
     }
 
+    public static boolean isvalidname(String name) {
+        String regex = "^Cap.{2,}$";
+        return Pattern.matches(regex, name);
 
-    public static boolean passwordAllEmailCheck(String password){
+    }
+    public static boolean validemail(String email){
         String regex = "^[a-zA-Z0-9]+[/.]?[a-zA-z0-9]*[@][a-z]{2,5}[/.][a-z]{2,3}[/.]?[a-z]*$";
-        return (Pattern.matches(regex, password));
-      }
+        return Pattern.matches(regex,email);
     }
+    public static boolean validnumber(String number){
+        String regex = "[0-9]{2}\\s[0-9]{10}";
+        return Pattern.matches(regex, number);
+    }
+    public static boolean validpassword(String password){
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-z0-9]).{8,}$";
+        return Pattern.matches(regex,password);
+    }
+}
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -7,19 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Passwordvalidator {
     @Test
-    void truepasswordTest() {
-        assertTrue(PasswordCheck.passwordAllEmailCheck("abc@yahoo.com"));
-        assertTrue(PasswordCheck.passwordAllEmailCheck("abc.100@yahoo.com"));
-        assertTrue(PasswordCheck.passwordAllEmailCheck("abc@gmail.com.com"));
-
+    public void checkfirstnametrue() {
+        assertTrue(PasswordCheck.isvalidname("Captain"));
+        assertFalse(PasswordCheck.isvalidname("Sujit"));
 
     }
+
     @Test
-   void falsepasswordTest() {
-        assertFalse(PasswordCheck.passwordAllEmailCheck("abc()*@gmail.com"));
-        assertFalse(PasswordCheck.passwordAllEmailCheck(".abc123@.com"));
-        assertFalse(PasswordCheck.passwordAllEmailCheck("abc..2002@gmail.com"));
+    public void checklastnamefalse() {
+        assertTrue(PasswordCheck.isvalidname("Captain"));
+        assertFalse(PasswordCheck.isvalidname("Sujit"));
+    }
 
+    @Test
+    public void checkemail() {
+        assertTrue(PasswordCheck.validemail("abc@yahoo.com"));
+        assertTrue(PasswordCheck.validemail("abc.100@yahoo.com"));
+        assertTrue(PasswordCheck.validemail("abc@gmail.com.com"));
+        assertFalse(PasswordCheck.validemail("abc()*@gmail.com"));
 
+    }
+
+    @Test
+    public void checknumber() {
+        assertTrue(PasswordCheck.validnumber("91 9082164605"));
+        assertFalse(PasswordCheck.validnumber("9082164605"));
+    }
+
+    @Test
+    public void checkpassword() {
+        assertTrue(PasswordCheck.validpassword("Sujitdhamke12@gmail.com"));
+        assertFalse(PasswordCheck.validpassword("sujit"));
     }
 }
